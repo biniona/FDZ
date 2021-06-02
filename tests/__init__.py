e064598cf8176dcc66d1eccb1dfb55989fda4810
Aleksander Binion <alek.binion@mongodb.com>
2021-06-01 22:38:45 -0400

    move cleanup into tests init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,25 @@
 """Unit test package for fdz."""
+import functools
+import os
+import shutil
+
+TEST_FILE_EVENTS_DIR = "__test_file_actions_dir__"
+
+
+def clean_up(func):
+    """wrapper for making a testing environment for file events"""
+    @functools.wraps(func)
+    def wrapper_cleanup(*args,**kwargs):
+        if os.path.exists(TEST_FILE_EVENTS_DIR):
+            raise ValueError("temporary test directory should not exist, abort testing")
+        current_directory = os.getcwd()
+        os.makedirs(TEST_FILE_EVENTS_DIR)
+        os.chdir(TEST_FILE_EVENTS_DIR)
+        print(current_directory)
+        try:
+            result = func(*args, **kwargs)
+            return result
+        finally:
+            os.chdir(current_directory)
+            shutil.rmtree(TEST_FILE_EVENTS_DIR)
+    return wrapper_cleanup
--- tests/test_file_actions.py
+++ tests/test_file_actions.py
@@ -4,34 +4,12 @@
 
 import os
 import shutil
-import functools
 
 import unittest
 
 from fdz import fdz
 from fdz import file_actions
-
-
-TEST_FILE_EVENTS_DIR = "__test_file_actions_dir__"
-
-
-def clean_up(func):
-    """creates an empty test dire"""
-    @functools.wraps(func)
-    def wrapper_cleanup(*args,**kwargs):
-        if os.path.exists(TEST_FILE_EVENTS_DIR):
-            raise ValueError("temporary test directory should not exist, abort testing")
-        current_directory = os.getcwd()
-        os.makedirs(TEST_FILE_EVENTS_DIR)
-        os.chdir(TEST_FILE_EVENTS_DIR)
-        print(current_directory)
-        try:
-            result = func(*args, **kwargs)
-            return result
-        finally:
-            os.chdir(current_directory)
-            shutil.rmtree(TEST_FILE_EVENTS_DIR)
-    return wrapper_cleanup
+from tests import clean_up
 
 class TestFileActions(unittest.TestCase):
     """Tests for `fdz.fileactions` functions."""
