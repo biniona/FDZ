08102653800d9e9c13474e9a74257161f778e521
Aleksander Binion <alek.binion@mongodb.com>
2021-06-05 15:15:50 -0400

    add docstrings for cli

--- fdz/cli.py
+++ fdz/cli.py
@@ -10,6 +10,7 @@ def cli():
 @cli.command(name = "d")
 @fa.check_is_init
 def daily():
+    """Make a new daily note. This is where you can keep throwaway notes by day."""
     path = fa.new_daily_note()
     if path:
         click.echo(f"New daily note: {path}")
@@ -18,12 +19,14 @@ def daily():
 
 @cli.command(name = "i")
 def init():
+    """Initialize your zettelkesten directory."""
     fa.safe_init()
 
 @cli.command(name = "z")
 @click.option('--card','-c', 'card', prompt=True, required=True, type=str)
 @fa.check_is_init
 def zettl(card):
+    """Make a new permanent note card. This is where you put a fully formed idea."""
     path = fa.new_zettl_note(*card.split("."))
     if path:
         click.echo(f"New zettl note: {path}")
@@ -36,6 +39,8 @@ def zettl(card):
 @click.option('--extra', '-e', 'extra', default="", required=False, type=str)
 @fa.check_is_init
 def bib(author, date, extra):
+    """Make a new bibliographic entry. This is where you keek
+    notes on a specific piece of media."""
     fa.new_bib_note(date, author, extra)
 
 if __name__ == "__main__":
